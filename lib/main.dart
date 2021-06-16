e2c0dbedff3682d21a4ba7d5d2949786ae3d665a
Jakub Zaremba <jzaremba@MAgiC.local>
2021-06-16 19:51:03 +0200

    [FLUT-3, FLUT-4] Simplify calc engine, generate layout by function Layout is now generated by function `buildKey` and `buildRowPad`. Calc engine: 	- checkIfNumeric - return true if given string is numeric, false if not 	- calculate - Validate the userInput and calculate result

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class SimpleCalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-            child: CalculatorCanvas()
-        ),
+        body: SafeArea(child: CalculatorCanvas()),
       ),
     );
   }
@@ -23,45 +21,128 @@ class CalculatorCanvas extends StatefulWidget {
 }
 
 class _CalculatorCanvasState extends State<CalculatorCanvas> {
+  String userInput1 = "";
+  String userInput2 = "";
+  String userSymbol = "";
+  String calcScreen = "";
 
-  Widget buildKey({required String keyValue})
-  => Expanded(
-    child: TextButton(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Colors.blue[200]),
-        ),
-        onPressed: () {},
-        child: Container(
-          child: Text("$keyValue"),
-        )
-    ),
+  bool checkIfNumeric({required String value}) {
+    try {
+      int.parse(value);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  void calculate({required String userInputValue}) {
+    setState(() {
+      if (checkIfNumeric(value: userInputValue) && userSymbol == "") {
+        userInput1 = userInput1 + userInputValue;
+        calcScreen = calcScreen + userInputValue;
+      } else if (checkIfNumeric(value: userInputValue) && userSymbol != "") {
+        userInput2 = userInput2 + userInputValue;
+        calcScreen = calcScreen + userInputValue;
+      } else if (!checkIfNumeric(value: userInputValue) &&
+          userSymbol == "" &&
+          userInputValue != "C" &&
+          userInputValue != "=") {
+        userSymbol = userInputValue;
+        calcScreen = calcScreen + userInputValue;
+      } else if (!checkIfNumeric(value: userInputValue) &&
+          userInputValue == "C") {
+        userInput1 = "";
+        userInput2 = "";
+        userSymbol = "";
+        calcScreen = "";
+      } else if (userInputValue == "=" &&
+          userInput1 != "" &&
+          userInput2 != "" &&
+          userSymbol != "") {
+        print("suma");
+        if (userSymbol == "+") {
+          calcScreen =
+              (int.parse(userInput1) + int.parse(userInput2)).toString();
+          userInput2 = "";
+          userSymbol = "";
+          userInput1 = calcScreen;
+        } else if (userSymbol == "-") {
+          calcScreen =
+              (int.parse(userInput1) - int.parse(userInput2)).toString();
+          userInput2 = "";
+          userSymbol = "";
+          userInput1 = calcScreen;
+        } else if (userSymbol == "*") {
+          calcScreen =
+              (int.parse(userInput1) * int.parse(userInput2)).toString();
+          userInput2 = "";
+          userSymbol = "";
+          userInput1 = calcScreen;
+        } else if (userSymbol == "/") {
+          calcScreen = ((int.parse(userInput1) / int.parse(userInput2)).round())
+              .toString();
+          userInput2 = "";
+          userSymbol = "";
+          userInput1 = calcScreen;
+        }
+      }
+    });
+  }
+
+  Widget buildKey({required String keyValue}) => TextButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.blue[100]),
+      ),
+      onPressed: () {
+        calculate(userInputValue: keyValue);
+      },
+      child: Container(
+        child: Text("$keyValue"),
+      )
   );
 
+  Widget buildRowPad(List<String> padNumbers) {
+    Widget rowPad = Expanded(
+      flex: 1,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(child: buildKey(keyValue: padNumbers[0])),
+          Expanded(child: buildKey(keyValue: padNumbers[1])),
+          Expanded(child: buildKey(keyValue: padNumbers[2])),
+          Expanded(child: buildKey(keyValue: padNumbers[3])),
+        ],
+      ),
+    );
+
+    return rowPad;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Expanded(
-          flex: 1,
+          flex: 2,
           child: Container(
+            alignment: Alignment.centerRight,
             width: double.infinity,
             color: Colors.yellow[50],
+            child: Text(
+              "$calcScreen",
+              style: TextStyle(fontSize: 25),
+            ),
           ),
         ),
         Expanded(
-          flex: 10,
+          flex: 15,
           child: Column(
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  buildKey(keyValue: "9"),
-                  buildKey(keyValue: "8"),
-                  buildKey(keyValue: "7"),
-                  buildKey(keyValue: "+"),
-                ],
-              ),
+              buildRowPad(["9", "8", "7", "+"]),
+              buildRowPad(["6", "5", "4", "-"]),
+              buildRowPad(["3", "2", "1", "*"]),
+              buildRowPad(["0", "=", "C", "/"]),
             ],
           ),
         ),
